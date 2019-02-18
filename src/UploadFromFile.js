69fa95688077b1c5ccbc8c907a9047ed23a2c011
Victor <vdiazroa@gmail.com>
2019-02-18 11:32:54 +0100

    changed alerts, and fixed regex

--- src/UploadFromFile.js
+++ src/UploadFromFile.js
@@ -4,36 +4,31 @@ export default class UploadFile extends TableGenerator {
     super(options);
     this.init();
   }
+
   init() {
     this.elements.container.innerHTML = this.parseHTML();
     this.elements.table = this.elements.container.querySelector(
       ".insert-table"
     );
-    this.alert();
+    this.elements.alert = this.elements.container.querySelector(".alert-csv");
     this.elements.container
       .querySelector(".form-control-file")
       .addEventListener("change", e => {
-        this.generateTableFromFile(e.target.files[0]);
+        this.readFile(e.target.files[0]);
       });
   }
-  generateTableFromFile(file) {
+
+  readFile(file) {
     const reader = new FileReader();
     reader.readAsText(file);
     reader.onload = file => this.formatData(file);
   }
-  next() {
-    this.collection = this.dataToObject(this.options.data);
-    this.elements.container.querySelector(
-      ".insert-filters"
-    ).innerHTML = this.parseFilters();
-    this.filterEvents();
-    this.generateTable();
-  }
+
   formatData(csv) {
     try {
       const text = csv.target.result.replace(/\t/g, ",").replace(/;/g, ",");
-      const regex = /([^\n,]+,)+[^\n,]+\n/g;
-      if (!regex.test(text)) throw Error;
+      const regex = /([^\n,]+\n)|([^\n,]+(,[^\n,])+\n)/g;
+      if (!regex.test(text)) throw new Error("Not Valid CSV file");
 
       this.options.data = text.split(/\r?\n/);
       while (this.options.data.slice(-1)[0] === "") this.options.data.pop();
@@ -43,48 +38,53 @@ export default class UploadFile extends TableGenerator {
       const checkLength = this.options.data.some(element => {
         return element.split(",").length !== titlesLenght;
       });
+      if (checkLength) throw new Error("Not Valid CSV file");
 
-      if (checkLength) throw Error;
-      this.elements.alertError.style.display = "none";
-      this.elements.alertSuccess.style.display = "block";
-      this.next();
+      this.alertUploaded();
+      this.generateTableFromFile();
     } catch (error) {
-      this.elements.alertSuccess.style.display = "none";
-      this.elements.alertError.style.display = "block";
+      this.alertError(error);
     }
   }
-  alert() {
-    this.elements.alertError = this.elements.container.querySelector(
-      ".alert-error"
-    );
-    this.elements.alertSuccess = this.elements.container.querySelector(
-      ".alert-uploaded"
-    );
-    this.elements.container
-      .querySelector(".close-error")
-      .addEventListener("click", e => {
-        this.elements.alertError.style.display = "none";
-      });
-    this.elements.container
-      .querySelector(".close-success")
-      .addEventListener("click", e => {
-        this.elements.alertSuccess.style.display = "none";
-      });
+
+  generateTableFromFile() {
+    this.collection = this.dataToObject(this.options.data);
+    this.elements.container.querySelector(
+      ".insert-filters"
+    ).innerHTML = this.parseFilters();
+    this.filterEvents();
+    this.generateTable();
   }
+
+  alertError(error) {
+    this.elements.alert.innerHTML = `
+    <div class="alert alert-danger top alert-error" role="alert">
+      <strong>ERROR!</strong> ${error.message}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>`;
+    this.elements.alert.querySelector(".close").addEventListener("click", e => {
+      this.elements.alert.innerHTML = "";
+    });
+  }
+
+  alertUploaded() {
+    this.elements.alert.innerHTML = `
+    <div class="alert alert-success top alert-uploaded" role="alert">
+      <strong>SUCCESS!</strong> CSV File has been successfully uploaded.
+      <button type="button" class=" close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div> `;
+    this.elements.alert.querySelector(".close").addEventListener("click", e => {
+      this.elements.alert.innerHTML = "";
+    });
+  }
+
   parseHTML() {
     return `
-    <div class="alert alert-success top alert-uploaded" role="alert" style="display:none">
-      <strong>SUCCESS!</strong> CSV File has been successfully uploaded.
-      <button type="button" class="close-success close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>    
-    <div class="alert alert-danger top alert-error" role="alert" style="display:none">
-      <strong>ERROR!</strong> Not Valid CSV file.
-      <button type="button" class="close-error close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>  
+    <div class="alert-csv"></div>
     <label for="csv">Choose a valid csv file:</label>
     <input type="file" id="csv" name="csv" accept=".csv" class="form-control-file mb-4">
     <span class="card border-secondary">
